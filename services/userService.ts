ecd61e81124e7fd60c3485b1c74f1f08967ee26e
ManuelFonte7 <manubusiness7@gmail.com>
2025-06-06 11:48:52 +0200

    caricamento immagine profilo

--- services/userService.ts
+++ services/userService.ts
@@ -1,12 +1,20 @@
 import { firestore } from "@/config/firebase";
 import { ResponseType, UserDataType } from "@/types";
 import { doc, updateDoc } from "firebase/firestore";
+import { uploadFileToCloudinary } from "./ImageService";
 
 export const updateUser = async (
     uid: string,
     updateData: UserDataType
 ): Promise<ResponseType> => {
     try{
+        if(updateData.image && updateData?.image?.uri){
+            const imageUploadRes = await uploadFileToCloudinary(updateData.image, "users");
+            if(!imageUploadRes.success){
+                return {success: false, msg: imageUploadRes.msg || "Caricamento immagine fallito"}
+            }
+            updateData.image = imageUploadRes.data;
+        }
         const userRef = doc(firestore, "users", uid);
         await updateDoc(userRef, updateData);
 
